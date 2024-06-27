576ba142e0f759471041f13bab8dc05be63c3161
Evie Frye <msmanika763348@gmail.com>
2024-06-27 14:12:35 +0530

    Added Images and few edits

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
 // For carousel
-const slides=document.querySelectorAll(".slide");
+const slides = document.querySelectorAll(".slide");
 //Here, we declared that slides will have all the images with have class slide.
-let counter=0;
+let counter = 0;
 //Here, I'm traversing each slide in slides and appling style.
-slides.forEach((slide,index)=>{
-    slide.style.left=`${index*100}%`
+slides.forEach((slide, index) => {
+    slide.style.left = `${index * 100}%`
 
 })
 //This is the sliding function.
-const slideImage=()=>{
+
+const slideImage = () => {
     slides.forEach(
-        (slide)=>{
-            slide.style.transform=`translateX(-${counter*100}%)`
+        (slide) => {
+            slide.style.transform = `translateX(-${counter * 100}%)`
         }
     )
 
 }
-const goNext=()=>{
-    counter++;
+const goNext = () => {
+    if(counter<slides.length-1){
+        counter++;
+    }else{
+        counter=0;
+    }
     slideImage();
 }
-const goPrev=()=>{
-    counter--;
+
+// If check whether the images is in range or not
+const goPrev = () => {
+    if(counter>0){
+        counter--;
+    }else{
+        counter.slides.length-1;
+    }
     slideImage();
 }
